be84d7f107f856b24863fbefa26e6f9a02e2184f
Owais <warsimuhammadowais@gmail.com>
2024-06-07 15:42:29 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ import { sendApplicationMail } from "./utilities/sendMail.js";
 import dotenv from "dotenv"
 dotenv.config();
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
 const app = express();
@@ -18,6 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 mongoose.connect(process.env.MONGO_URL);
 
